639682bf0c7e2f249f75a85e98ed0f0709a67586
Iqbal Maulana <94213713+IqbalMaulana7@users.noreply.github.com>
2021-11-13 13:57:31 +0700

    Add files via upload

---
+++ Tugas Mas Zidni.py
@@ -0,0 +1,7 @@
+Nama = "Iqbal Maulana Kholidi"
+NIM = "212410102011"
+Kelompok = "Kelompok 1"
+Asal_Kota = "Kota Jember"
+Program_studi = "Teknologi Informasi"
+print("=====PERKENALAN=====")
+print (f"Nama = {Nama}\nNIM = {NIM}\nKelompok ={Kelompok}\nAsal_Kota = {Asal_Kota}\nPrograms Studi = {Program_studi}")
